5e87a567bc35457db7e981fe3707b509264085ed
Ivan Sergiienko <darthvanger@gmail.com>
2022-06-17 01:27:28 +0300

    Add controls! :)

--- game/game.js
+++ game/game.js
@@ -1,1 +1,31 @@
-console.log('hello');
+const ship = document.querySelector('#ship-translator');
+const rotator = document.querySelector('#ship-rotator');
+
+let x = 0;
+let y = 0;
+
+const step = 20;
+
+document.addEventListener('keydown', e => {
+  console.log('keydown');
+  if (e.key == 'ArrowUp') {
+    y -= step;
+    ship.style.top = y;
+    rotator.className = 'up';
+  }
+  if (e.key == 'ArrowDown') {
+    y += step;
+    ship.style.top = y;
+    rotator.className = 'down';
+  }
+  if (e.key == 'ArrowLeft') {
+    x -= step;
+    ship.style.left = x;
+    rotator.className = 'left';
+  }
+  if (e.key == 'ArrowRight') {
+    rotator.className = 'right';
+    x += step;
+    ship.style.left = x;
+  }
+});
