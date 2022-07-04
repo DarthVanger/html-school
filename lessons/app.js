5f01a5526289b7def559377ae3a038a75db69b64
Ivan Sergiienko <darthvanger@gmail.com>
2022-07-04 21:14:34 +0300

    Add lessons page

---
+++ lessons/app.js
@@ -0,0 +1,33 @@
+const app = document.querySelector('#app');
+const lessons = [
+  {
+    url: 'conditionals',
+    img: '<img src="conditionals.jpeg" />',
+    title: 'conditionals',
+  },
+  {
+    url: 'conditionals',
+    img: '<img src="conditionals.jpeg" />',
+    title: 'conditionals',
+  },
+];
+
+const lessonsList = lessons.map(lesson => `
+  <li>
+    <a href="${lesson.url}">
+      <figure>
+        ${lesson.img}
+        <figcaption>
+          ${lesson.title}   
+        </figcatption>
+      <figure>
+    </a>
+  </li>
+`);
+
+app.innerHTML = `
+  <ul>
+    ${lessonsList.join('')}
+  </ul>
+`;
+
---
+++ lessons/conditions.js
@@ -0,0 +1,4 @@
+console.log('consitions.js');
+export default function() {
+  console.log('ConditionsLesson :)');
+}
