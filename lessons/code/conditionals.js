a749059c842c8c734315d309fa33371fadea6214
Ivan Sergiienko <darthvanger@gmail.com>
2022-07-07 01:30:16 +0300

    Add a task for conditionals

--- lessons/code/conditionals.js
+++ lessons/code/conditionals.js
@@ -8,16 +8,50 @@ export default `
   </p> 
 
   <h2>Task</h2>
+  <p>
+    The script also has a click handler.
+  </p>
+  <p>
+    It shows event.pageX in the alert popup.
+  </p>
+  <p>
+    Click the page to try it out.
+  </p>
+  <p>
+    Your task to make the alert appear only when event.pageX is more than 100.
+  </p>
+  <p>
+    <pre><code>
+      if (event.pageX > 100) {
+        alert('PageX is > 100';
+      } else {
+        alert('PageY is < 100');
+      }
+    </code></pre>
+  </p>
+    
 
 </body>
 
 <script>
+  /**
+   * Example
+   */
   var x = 'xuy4ek';
 
   if (x == 'xuy4ek') {
-    alert('X - eto Xuy4eK!');
+    console.log('X - eto Xuy4eK!');
   } else {
-    alert('X - HE XUY!');
+    console.log('X - HE XUY!');
   }
+
+  /**
+   * Task
+   */
+  function handleClick(event) {
+    alert('PageX is: ' + event.pageX);
+  }
+
+  document.addEventListener('click', handleClick)
 </script>
 `;
