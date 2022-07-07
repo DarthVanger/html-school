2bd6b2e64df3b8cabfe79648d10f0e1d670391c2
Ivan Sergiienko <ivan@freedomfi.com>
2022-07-08 02:46:30 +0300

    Render two lesson chains

--- lessons/app.js
+++ lessons/app.js
@@ -9,37 +9,64 @@ const lessons = [
       img: '<img src="img/walls.gif" />',
       title: 'walls task',
     },
+  ],
+  [
+    {
+      img: '<img src="img/walls.gif" />',
+      title: 'walls example',
+    },
+    {
+      img: '<img src="img/walls.gif" />',
+      title: 'walls task',
+    },
   ]
 ];
 
 const Lesson = lesson => `
-  <li>
+  <div class="lesson">
     <a href="lesson.html#${lesson.title}">
       <figure>
         ${lesson.img}
-        <figcaption>
-          ${lesson.title}   
-        </figcatption>
       <figure>
     </a>
-  </li>
+  </div>
 `;
+
+const Arrow = () => {
+  const r = 15;
+  const h = 150;
+  const w = 10;
+
+  return `
+    <svg width="${2 * r}" height="${h}">
+      <circle cx="${r}" cy="${r}" r="${r}" />
+      <line x1="${r}" y1="${r}" x2="${r}" y2="${r + h}" stroke-width="${w}" />
+      <circle cx="${r}" cy="${h - r}" r="${r}" />
+    </svg>
+  `;
+}
 
 const LessonChain = lessonChain => {
   let html = '';
   for (let lesson of lessonChain) {
     html += Lesson(lesson);
+    html += Arrow();
   }
   return html;
 }
 
+const LessonsGrid = lessons => {
+  let html = '<div class="lessons-grid">';  
+  for (let lessonChain of lessons) {
+    html +='<div class="col">';
+    html += LessonChain(lessonChain);
+    html += '</div>';
+  }
+  html += '</div>';
+  return html;
+};
+
 const lessonsList = lessons.map(LessonChain);
 
 
-
-app.innerHTML = `
-  <ul>
-    ${lessonsList.join('')}
-  </ul>
-`;
-
+app.innerHTML = LessonsGrid(lessons);
