0a5c90ff792c0df192598808a77d5bb7b0d1e9ca
Ivan Sergiienko <ivan@freedomfi.com>
2022-07-21 00:49:06 +0300

    Don't update mentor img on render

--- quests/mentor.js
+++ quests/mentor.js
@@ -8,15 +8,52 @@ const getTextContent = (editor) => {
   return editor.textContent;
 };
 
-const getButtonText = () => 'Закройся';
-
 const getCode = () => getInnerText(getEditor());
+
+const parseCode = str => {
+  let result = str;
+  result = result.replaceAll('|c|', '<pre><code>');
+  result = result.replaceAll('|ce|', '</code></pre>');
+  return result;
+}
+
+
+const MentorMessage = ({ state, quest }) => `
+  ${state.isQuestCompleted && Congrats({ quest }) || ''}
+  ${getStepText({ state, quest })}
+`;
+ 
+const getStepText = ({ state, quest }) => {
+  if (state.step === quest.steps.length) {
+    return '';
+  }
+
+  const task = quest.steps[state.step].task;
+  return parseCode(task);
+}
+
+const Congrats = ({ quest }) => `
+  <div>
+    <span>Принято</span>
+    <span class="icon">✔</span>
+    Прокачаны скилы:
+  </div>
+  <div class="achievements">
+    ${quest?.skills?.map(skill => `
+      <div class="achievement">
+        <img src="img/medal.png" />
+        ${skill}
+      </div>
+    `).join('')}
+  </div>
+`;
 
 export const Mentor = (quest) => {
   const { steps } = quest;
   
   const element = document.createElement('div');
   element.id = 'mentor';
+  const getElement = () => document.querySelector('#mentor');
 
   const state = {
     step: 0,
@@ -26,7 +63,7 @@ export const Mentor = (quest) => {
   const setState = (newState) => {
     state.step = newState.step;
     state.isQuestCompleted = newState.isQuestCompleted;
-    render();
+    update();
   };
 
   const check = () => {
@@ -89,38 +126,6 @@ export const Mentor = (quest) => {
     ).join('');
   };
 
-  const parseCode = str => {
-    let result = str;
-    result = result.replaceAll('|c|', '<pre><code>');
-    result = result.replaceAll('|ce|', '</code></pre>');
-    return result;
-  }
-
-  const getStepText = () => {
-    if (state.step === steps.length) {
-      return '';
-    }
-
-    const task = steps[state.step].task;
-    return parseCode(task);
-  }
-
-  const Congrats = () => `
-    <div>
-      <span>Принято</span>
-      <span class="icon">✔</span>
-      Прокачаны скилы:
-    </div>
-    <div class="achievements">
-      ${quest?.skills?.map(skill => `
-        <div class="achievement">
-          <img src="img/medal.png" />
-          ${skill}
-        </div>
-      `).join('')}
-    </div>
-  `;
-
   const render = () => {
     console.log('render, state: ', state);
     const imgSrc = state.isQuestCompleted ?
@@ -130,14 +135,22 @@ export const Mentor = (quest) => {
     element.innerHTML = `
       <div id="napaleon">
         <img id="napaleon-img" src="${imgSrc}" />
-        <div id="napaleon-message">
-          ${state.isQuestCompleted && Congrats() || ''}
-          ${getStepText()}
-        </div>
+          <div id="napaleon-message">
+            ${MentorMessage({ state, quest })}
+          </div>
       </div>
       <div id="steps-progress">
         ${generateStepsHTML()}
       </div>
+    `;
+  };
+
+  const update = () => {
+    document.querySelector('#napaleon-message').innerHTML = `
+      ${MentorMessage({ state, quest })}
+    `;
+    document.querySelector('#steps-progress').innerHTML = `
+      ${generateStepsHTML()}
     `;
   };
 
