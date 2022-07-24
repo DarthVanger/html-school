2a4f3fd3ed4f9f051348076af67f4b16a19ebc91
Ivan Sergiienko <ivan@freedomfi.com>
2022-07-24 21:23:03 +0300

    Add video and img to html lesson :)

---
+++ src/quests/Html/subtitles.js
@@ -0,0 +1,23 @@
+export const subtitles `
+  >> Last month I was off in Boston for the
+  fourth annual World Wide Web Conference, and had the opportunity to talk to
+  the inventor of the World Wide Web, Tim Berners-Lee. And I asked him a little bit about
+  where are we going with this web stuff. And we have just a little bit
+  of footage from Tim telling us where that's going to be. >> You haven't seen nothing yet. But you wait until people assume the web. When the web becomes something
+  you can assume, when the web or the infrastructure is all laid down and
+  we have an information space, then it will be time for the next revolution
+  >> It's hard to imagine a, a greater evolution than what we've seen,
+  I mean, I, I go to the smallest places, and
+  everybody's putting up a web page. >> Right, but
+  the web could only take place, the revol, the web revolution could only
+  take place because the internet, which would be had been a quieter,
+  smaller revolution, but that had been of the Internet itself quietly being deployed
+  throughout the world had happened. Because the Internet was
+  something people could assume, the Web revolution could take place. When the Web is something you can assume,
+  then maybe a cultural revolution, and I say cultural revolution. We need cultural revolution perhaps or maybe we'll find ways of doing things
+  better that we haven't imagined. >> Pretty neat, Rich. One of the things that I, I liked about
+  it was he, he just said, we're going to, his idea was, the next step is
+  something completely different, a cultural revolution as compared
+  to just a little more technology. And he, Tim Berners-Lee being from Europe, I can sort of see how he
+  can have that perspective
+`;
