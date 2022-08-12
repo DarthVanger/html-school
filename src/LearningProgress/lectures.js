665538897103e9f0afdacfece021e330899e94b9
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-13 02:17:16 +0300

    Remove localhost from slides urls :)

--- src/LearningProgress/lectures.js
+++ src/LearningProgress/lectures.js
@@ -2,22 +2,22 @@ export const lectures = [
   {
     text: 'ЗНОКОМСТВО - HTML: <h1>, <p>, <img>. CSS: h1, p.',
     skills: ['html-h1', 'html-p', 'html-img', 'css-h1-p', 'css-color'],
-    url: 'http://localhost:8080/src/slides.html#lesson1',
+    url: '/src/slides.html#lesson1',
   },
   {
     text: 'ССЫЛКЕ: <a href="...">, ВИДЕО: <video>',
     skills: ['html-a', 'html-video'],
-    url: 'http://localhost:8080/src/slides.html#lesson2',
+    url: '/src/slides.html#lesson2',
   },
   {
     text: 'ДЖАВО-СКРИПТ',
     skills: ['functions', 'variables', 'querySelector', , 'style', 'alert', 'xuy4ek'],
-    url: 'http://localhost:8080/src/slides.html#lesson3',
+    url: '/src/slides.html#lesson3',
   },
   {
     text: 'ПОЛЕТ КОРАБЛЯ',
     skills: ['event', 'on click', 'console', 'functions', 'click-event', 'variables', 'style'],
-    url: 'http://localhost:8080/src/slides.html#lesson4',
+    url: '/src/slides.html#lesson4',
   },
   {
     text: 'СТЕНЫ: if/else. ПЛАЗМА',
