a7ebbd936d19c587d33e638930557d88fcf8c0fb
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-18 02:58:56 +0300

    Add expressions lecture video and slide

--- src/slides/lesson11/burn.sh
+++ src/slides/lesson11/burn.sh
@@ -1,12 +1,12 @@
 source ~/.bash_profile
 
-video=source_video/img-lecture/img-lecture.mp4
-subs=source_video/img-lecture/sub.vtt
+video=source_video/expressions/expressions-lecture.mp4
+subs=source_video/expressions/sub.vtt
 subs_ass=out/sub.ass
 subs_srt=out/sub.srt
 
 ffmpeg -i $subs $subs_ass
 ffmpeg -i $subs $subs_srt
 
-ffmpeg -y -i "${video}" -vf "subtitles=${subs_srt}:force_style='Fontsize=24,PrimaryColour=&H333333&BackColour=&H333333&'" -c:a copy out.mp4
+ffmpeg -y -i "${video}" -vf "subtitles=${subs_srt}:force_style='Fontsize=24,PrimaryColour=&H333333&BackColour=&HEEEEEE&'" -c:a copy out.mp4
 #ffmpeg -y -i "${video}" -vf "ass=${subs_ass}" -c:a copy out.mp4
