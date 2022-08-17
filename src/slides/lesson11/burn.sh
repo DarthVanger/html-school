bbbf1d5802d30bc718ca37713949763856fb8bbc
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-18 02:51:24 +0300

    Create video for img lecture

--- src/slides/lesson11/burn.sh
+++ src/slides/lesson11/burn.sh
@@ -1,1 +1,12 @@
-ffmpeg -y -i source-variables.mp4 -vf "subtitles=sub.srt:force_style='Fontsize=24,PrimaryColour=&HEEEEEE&BackColour=&H333333&'" -c:a copy out.mp4
+source ~/.bash_profile
+
+video=source_video/img-lecture/img-lecture.mp4
+subs=source_video/img-lecture/sub.vtt
+subs_ass=out/sub.ass
+subs_srt=out/sub.srt
+
+ffmpeg -i $subs $subs_ass
+ffmpeg -i $subs $subs_srt
+
+ffmpeg -y -i "${video}" -vf "subtitles=${subs_srt}:force_style='Fontsize=24,PrimaryColour=&H333333&BackColour=&H333333&'" -c:a copy out.mp4
+#ffmpeg -y -i "${video}" -vf "ass=${subs_ass}" -c:a copy out.mp4
--- src/slides/lesson11/cut.sh
+++
@@ -1,7 +0,0 @@
-rm -rf out/*
-ffmpeg -ss 0 -i snoop.mp4 -t 4 out/snoop-millionare-start.mp4
-ffmpeg -ss 44.5 -i snoop.mp4 -t 3.01 out/snoop-millionare-hello.mp4
-ffmpeg -ss 85 -i snoop.mp4 -t 0.5 out/snoop-wow.mp4
-ffmpeg -ss 00:01:41.9 -i snoop.mp4 -t 1.1 out/snoop-snoopy.mp4
-ffmpeg -ss 00:03:47 -i snoop.mp4 -t 5 out/snoop-man-i-seen.mp4
-ffmpeg -ss 00:05:34 -i snoop.mp4 -t 5 out/snoop-wins.mp4
