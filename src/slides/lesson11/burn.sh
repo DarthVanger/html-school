68479fb3c6e09fa7f25d8491474d9d0734589bd1
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-18 03:03:55 +0300

    Add lecture for changing page interactively

--- src/slides/lesson11/burn.sh
+++ src/slides/lesson11/burn.sh
@@ -1,12 +1,12 @@
 source ~/.bash_profile
 
-video=source_video/expressions/expressions-lecture.mp4
-subs=source_video/expressions/sub.vtt
+video=source_video/changing-pages-interactively/changing-pages-interactively.mp4
+subs=source_video/changing-pages-interactively/sub.vtt
 subs_ass=out/sub.ass
 subs_srt=out/sub.srt
 
-ffmpeg -i $subs $subs_ass
-ffmpeg -i $subs $subs_srt
+#ffmpeg -i -y $subs $subs_ass
+ffmpeg -i -y $subs $subs_srt
 
 ffmpeg -y -i "${video}" -vf "subtitles=${subs_srt}:force_style='Fontsize=24,PrimaryColour=&H333333&BackColour=&HEEEEEE&'" -c:a copy out.mp4
 #ffmpeg -y -i "${video}" -vf "ass=${subs_ass}" -c:a copy out.mp4
