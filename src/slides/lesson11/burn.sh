ae75a9e717dac12c29b773ac944f349203a6a1eb
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-18 14:22:45 +0300

    Add contitionals lecture video

--- src/slides/lesson11/burn.sh
+++ src/slides/lesson11/burn.sh
@@ -1,12 +1,12 @@
 source ~/.bash_profile
 
-video=source_video/changing-pages-interactively/changing-pages-interactively.mp4
-subs=source_video/changing-pages-interactively/sub.vtt
+video=source_video/conditionals/conditionals-lecture.mp4
+subs=source_video/conditionals/sub.vtt
 subs_ass=out/sub.ass
 subs_srt=out/sub.srt
 
-#ffmpeg -i -y $subs $subs_ass
-ffmpeg -i -y $subs $subs_srt
+ffmpeg -y -i $subs $subs_srt
+#ffmpeg -y -i $subs $subs_ass
 
 ffmpeg -y -i "${video}" -vf "subtitles=${subs_srt}:force_style='Fontsize=24,PrimaryColour=&H333333&BackColour=&HEEEEEE&'" -c:a copy out.mp4
 #ffmpeg -y -i "${video}" -vf "ass=${subs_ass}" -c:a copy out.mp4
---
+++ src/slides/lesson11/cut-conditionals.sh
@@ -0,0 +1,1 @@
+ffmpeg -i out.mp4 -ss 27 -t 212 out/cut.mp4
