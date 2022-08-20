e2a17fb36082613f81d52b065df01ad7e9d5ad4d
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-20 22:57:23 +0300

    Add snoop video to the Quiz page :)

--- src/Quiz/QuizPage.js
+++ src/Quiz/QuizPage.js
@@ -1,8 +1,16 @@
 const element = document.createElement('div');
+element.id = "quiz-page";
 
 export const QuizPage = () => {
   element.innerHTML = `
     <h2>Quiz Time</h2>
+    <figure>
+      <video autoplay muted loop>
+        <source src="/video/snoop/loop/start.mp4" type="video/mp4" />
+      </video>
+    </figure>
   `;
+
+  
   return element;
 };
