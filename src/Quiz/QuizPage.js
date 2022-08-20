22392ac966393a7e6d3d7b5ed32c661af9f6e21c
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-21 02:25:57 +0300

    Add variables lecture to the quiz

--- src/Quiz/QuizPage.js
+++ src/Quiz/QuizPage.js
@@ -9,8 +9,22 @@ export const QuizPage = () => {
         <source src="/video/snoop/loop/start.mp4" type="video/mp4" />
       </video>
     </figure>
-  `;
+    <h3>Variables (рус. - перменные, укр. - змінні, сурж. - перменні)</h3>
 
+    <figure>
+      <video controls>
+      <source src="/video/variables-lecture.mp4" type="video/mp4" />
+      </video>
+      <figcaption>Variables lecture</figcaption>
+    </figure>
+
+    <figure>
+      <video controls>
+      <source src="/video/expressions-lecture.mp4" type="video/mp4" />
+      </video>
+      <figcaption>Expressions lecture</figcaption>
+    </figure>
+  `;
   
   return element;
 };
