887cfb37ba73fc6ffb3a8be44d501523b8e60bf1
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-21 04:31:34 +0300

    Add plan for Quiz questions

--- src/Quiz/Variables/Variables.js
+++ src/Quiz/Variables/Variables.js
@@ -11,6 +11,19 @@ export const Variables = () => {
       let z = x + 2 * 4;
     </code></pre>
     <p>Which of the following correctly depicts the state of the program after these statements are executed?
+
+    <h3>Фізика 8й клас</h3>
+    <!-- A picture of 8 class kids xD -->
+    <!-- video of school: from the movie or youtube: meme about nicknames in Zoom :) -->
+    <!-- A page from ukr book -->
+    <!-- A problem from ukr book: vehicle goes with friction -->
+    <article>
+    </article>
+
+    <h3>Physics</h3>
+    <!-- MIT / Harvard / etc good free book with explanation and problem -->
+    <article>
+    </article>
   `;
 
   return element;
