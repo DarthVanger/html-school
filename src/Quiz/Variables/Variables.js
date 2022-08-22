624acbfec182a598e6bbb34635f5fd382ea133ca
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-22 13:45:55 +0300

    Add submit button for Quiz

--- src/Quiz/Variables/Variables.js
+++ src/Quiz/Variables/Variables.js
@@ -2,14 +2,30 @@ import { AnswerVariants } from '../AnswerVariants.js';
 const element = document.createElement('div');
 element.className = 'quiz';
 
+const correctAnswers = [0, 1, 2, 3];
+
 let state = {
-  questions: [0, 0, 0, 0],
+  questions: [-1, -1, -1, -1],
+  results: [],
 };
 
 export const Variables = () => {
+  const handleQuizSubmit = () => {
+    console.info('handleQuizSubmit');
+    let i = 0;
+    for (let q of state.questions) {
+      console.debug('q: ', q);
+      state.results[i] = (q === correctAnswers[i]);
+    }
+    console.info('results', state.results);
+  };
+
   setTimeout(() => {
     const codes = document.querySelectorAll('.quiz code');
     codes.forEach(c => Prism.highlightElement(c));
+
+    const submitButton = document.querySelector('#quiz-submit');
+    submitButton.addEventListener('click', handleQuizSubmit);
   });
 
   const handleVariantChange = ({question, selectedVariant }) => {
@@ -37,8 +53,11 @@ export const Variables = () => {
         ${AnswerVariants({
           selectedVariant: state.questions[0],
           onChange: (selectedVariant) => handleVariantChange({ question: 0, selectedVariant }),
+          isCorrect: state.results[0],
         })}
       </article>
+
+      <button type="button" id="quiz-submit">SUBMIT</button>
 
       <h3>Фізика 8й клас</h3>
       <!-- A picture of 8 class kids xD -->
