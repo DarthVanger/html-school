a3cf8cf0f51eef03cecc71596f34dcc5982b53b4
Ivan Sergiienko <ivan@freedomfi.com>
2022-08-29 13:36:03 +0300

    Add sync-db script

---
+++ sync-db.sh
@@ -0,0 +1,10 @@
+server_path=/root/html-school/server/db/db.json
+local_path=./server/db/db.json
+
+# https://askubuntu.com/a/715432
+date="$(date +%Y%m%d_%H%M%S)"
+backup_folder="backup/$date"
+mkdir -p backup_folder
+mv "$local_path" "$backup_folder"
+
+scp root@159.223.238.99:$server_path "$local_path"
