364175efcaae23fab991ce2298d77dcac9c8cc97
Ivan Sergiienko <ivan@freedomfi.com>
2022-09-11 19:33:58 +0300

    Add all neural art images

--- src/NeuralArt/NeuralArt.js
+++ src/NeuralArt/NeuralArt.js
@@ -4,20 +4,98 @@ export const NeuralArt = () => {
       <h1>NeuraL ArT</h1>
       <div class="grid">
         <figure>
-          <img src="src/NeuralArt/img/dimon-1.jpg" />
+          <img src="src/NeuralArt/img/dimon/bavovnyatko-1.jpg" />
           <figcaption>Dimon Da Vinci</figcaption>
         </figure>
         <figure>
-          <img src="src/NeuralArt/img/johnny-jedi-1.jpg" />
+          <img src="src/NeuralArt/img/dimon/bavovnyatko-2.jpg" />
+          <figcaption>Dimon Da Vinci</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/johnny/concept-art-1.jpg" />
           <figcaption>Johnny Ke Nobi</figcaption>
         </figure>
         <figure>
-          <img src="src/NeuralArt/img/johnny-concept-art-1.jpg" />
-          <figcaption>Johnny La Planshe</figcaption>
+          <img src="src/NeuralArt/img/johnny/concept-art-2.jpg" />
+          <figcaption>Johnny Ke Nobi</figcaption>
         </figure>
         <figure>
-          <img src="src/NeuralArt/img/van4ik-cyberpunk-weed-1.jpg" />
+          <img src="src/NeuralArt/img/johnny/fallout-1.jpg" />
+          <figcaption>Johnny Ke Nobi</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/johnny/fallout-2.jpg" />
+          <figcaption>Johnny Ke Nobi</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/johnny/fallout-logo-1.jpg" />
+          <figcaption>Johnny Ke Nobi</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/johnny/it-school-1.jpg" />
+          <figcaption>Johnny Ke Nobi</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/johnny/jedi-1.jpg" />
+          <figcaption>Johnny Ke Nobi</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/johnny/jedi-2.jpg" />
+          <figcaption>Johnny Ke Nobi</figcaption>
+        </figure>
+
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/cyberpunk-rasta-1.jpg" />
           <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-cyberpunk-1.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-cyberpunk-2.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-mj/01.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-mj/02.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-mj/03.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-mj/04.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-mj/05.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-mj/06.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-mj/07.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/van4ik/logo-mj/08.jpg" />
+          <figcaption>Van4ik Da Vin4ik</figcaption>
+        </figure>
+
+        <figure>
+          <img src="src/NeuralArt/img/harvard-1.jpg" />
+          <figcaption>Students :)</figcaption>
+        </figure>
+        <figure>
+          <img src="src/NeuralArt/img/harvard-2.jpg" />
+          <figcaption>Students :)</figcaption>
         </figure>
       </div>
     </article>
