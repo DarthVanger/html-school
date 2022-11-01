b1a46be03dd8d2f6629dd735623044ba1d1c1937
Ivan Sergiienko <ivan@freedomfi.com>
2022-11-01 04:43:13 +0200

    Improve styles for Profile 3d avatar :)

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -2,6 +2,13 @@ import { Avatar3d } from './Avatar3d.js';
 
 export const Profile = () => {
   return `
-    ${Avatar3d()}
+    <div id="profile">
+      <div class="profile-avatar">
+        ${Avatar3d()}
+      </div>
+      <div class="profile-skills">
+        ${Avatar3d()}
+      </div>
+    </div>
   `;
 };
