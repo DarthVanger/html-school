01c1abe9c366f9555421310105f3c14e23ebbcc2
Ivan Sergiienko <ivan@freedomfi.com>
2022-11-01 06:12:18 +0200

    Show Dude with student photos xD

--- src/Profile/Avatar3d.js
+++ src/Profile/Avatar3d.js
@@ -30,65 +30,78 @@ export const Avatar3d = () => {
 
     var createScene = function () {
 
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
-	scene.clearColor = BABYLON.Color3.Black();
-
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.ArcRotateCamera("camera1", - Math.PI / 3, 5 * Math.PI / 12, 50, new BABYLON.Vector3(0, 5, 0), scene);
-
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+      // This creates a basic Babylon Scene object (non-mesh)
+      var scene = new BABYLON.Scene(engine);
+      scene.clearColor = BABYLON.Color3.Black();
     
-	var defaultGridMaterial = new BABYLON.GridMaterial("default", scene);
-	defaultGridMaterial.majorUnitFrequency = 5;
-	defaultGridMaterial.gridRatio = 0.5;
-	
-	var sphere = BABYLON.Mesh.CreateSphere("sphere", 20, 9, scene);
-	sphere.position.y = 12;
-	sphere.position.x = -6;
-	sphere.material = defaultGridMaterial;
-	
- 	var knotMaterial = new BABYLON.GridMaterial("knotMaterial", scene);
-	knotMaterial.majorUnitFrequency = 8;
-	knotMaterial.minorUnitVisibility = 0.45;
-	knotMaterial.gridRatio = 0.3;
-	knotMaterial.mainColor = new BABYLON.Color3(0, 0, 0);
-	knotMaterial.lineColor = new BABYLON.Color3(0.0, 1.0, 0.0);
-		
- 	var knot = BABYLON.Mesh.CreateTorusKnot("knot", 3, 1, 128, 64, 2, 3, scene);
-	knot.position.y = 12.0;
-	knot.position.x = 6;
-	knot.material = knotMaterial;
+      // This creates and positions a free camera (non-mesh)
+      var camera = new BABYLON.ArcRotateCamera("camera1", - Math.PI / 3, 5 * Math.PI / 12, 50, new BABYLON.Vector3(0, 5, 0), scene);
 
-    var groundMaterial = new BABYLON.GridMaterial("groundMaterial", scene);
-	groundMaterial.majorUnitFrequency = 5;
-	groundMaterial.minorUnitVisibility = 0.45;
-	groundMaterial.gridRatio = 2;
-	groundMaterial.backFaceCulling = false;
-	groundMaterial.mainColor = new BABYLON.Color3(1, 1, 1);
-	groundMaterial.lineColor = new BABYLON.Color3(1.0, 1.0, 1.0);
-	groundMaterial.opacity = 0.98;
+      const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 5, 0));
+    
+      // This attaches the camera to the canvas
+      camera.attachControl(canvas, true);
+        
+      var defaultGridMaterial = new BABYLON.GridMaterial("default", scene);
+      defaultGridMaterial.majorUnitFrequency = 5;
+      defaultGridMaterial.gridRatio = 0.5;
+      //
+      var sphereMaterial = new BABYLON.StandardMaterial("Sphere Material", scene);
+      sphereMaterial.diffuseTexture = new BABYLON.Texture("/img/tony.jpg", scene);
+    
+      //var sphere = BABYLON.Mesh.CreateSphere("sphere", 20, 9, scene);
+     BABYLON.SceneLoader.ImportMeshAsync("him", "/src/Profile/Dude/", "Dude.babylon", scene).then((result) => {
+        var sphere = result.meshes[0];
+        sphere.scaling = new BABYLON.Vector3(0.25, 0.25, 0.25);
+                
+        scene.beginAnimation(result.skeletons[0], 0, 100, true, 1.0);
 
-	var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "textures/heightMap.png", 100, 100, 100, 0, 10, scene, false);
-	ground.material = groundMaterial;
+        sphere.position.y = 0;
+        sphere.position.x = -6;
+        sphere.material = sphereMaterial; 
+    });
 
-	var skyMaterial = new BABYLON.GridMaterial("skyMaterial", scene);
-	skyMaterial.majorUnitFrequency = 6;
-	skyMaterial.minorUnitVisibility = 0.43;
-	skyMaterial.gridRatio = 0.5;
-	skyMaterial.mainColor = new BABYLON.Color3(0, 0.05, 0.2);
-	skyMaterial.lineColor = new BABYLON.Color3(0, 1.0, 1.0);	
-	skyMaterial.backFaceCulling = false;
-	
-	var skySphere = BABYLON.Mesh.CreateSphere("skySphere", 30, 110, scene);
-	skySphere.material = skyMaterial;
-	
-	engine.runRenderLoop(function () {
-		camera.alpha += 0.003;
-	});	
-		
-    return scene;
+    
+      var knotMaterial = new BABYLON.GridMaterial("knotMaterial", scene);
+      knotMaterial.majorUnitFrequency = 8;
+      knotMaterial.minorUnitVisibility = 0.45;
+      knotMaterial.gridRatio = 0.3;
+      knotMaterial.mainColor = new BABYLON.Color3(0, 0, 0);
+      knotMaterial.lineColor = new BABYLON.Color3(0.0, 1.0, 0.0);
+        
+      var knot = BABYLON.Mesh.CreateTorusKnot("knot", 3, 1, 128, 64, 2, 3, scene);
+      knot.position.y = 12.0;
+      knot.position.x = 6;
+      knot.material = knotMaterial;
+  
+      var groundMaterial = new BABYLON.GridMaterial("groundMaterial", scene);
+      groundMaterial.majorUnitFrequency = 5;
+      groundMaterial.minorUnitVisibility = 0.45;
+      groundMaterial.gridRatio = 2;
+      groundMaterial.backFaceCulling = false;
+      groundMaterial.mainColor = new BABYLON.Color3(1, 1, 1);
+      groundMaterial.lineColor = new BABYLON.Color3(1.0, 1.0, 1.0);
+      groundMaterial.opacity = 0.98;
+    
+      var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "textures/heightMap.png", 100, 100, 100, 0, 10, scene, false);
+      ground.material = groundMaterial;
+  
+      var skyMaterial = new BABYLON.GridMaterial("skyMaterial", scene);
+      skyMaterial.majorUnitFrequency = 6;
+      skyMaterial.minorUnitVisibility = 0.43;
+      skyMaterial.gridRatio = 0.5;
+      skyMaterial.mainColor = new BABYLON.Color3(0, 0.05, 0.2);
+      skyMaterial.lineColor = new BABYLON.Color3(0, 1.0, 1.0);	
+      skyMaterial.backFaceCulling = false;
+      
+      var skySphere = BABYLON.Mesh.CreateSphere("skySphere", 30, 110, scene);
+      skySphere.material = skyMaterial;
+      
+      engine.runRenderLoop(function () {
+        camera.alpha += 0.003;
+      });	
+
+      return scene;
     };
 
     const initFunction = async function() {
@@ -116,7 +129,7 @@ export const Avatar3d = () => {
     };
 
     initFunction().then(() => {
-      sceneToRender = scene                    
+      sceneToRender = scene;
     });
 
     // Resize
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -3,12 +3,7 @@ import { Avatar3d } from './Avatar3d.js';
 export const Profile = () => {
   return `
     <div id="profile">
-      <div class="profile-avatar">
-        ${Avatar3d()}
-      </div>
-      <div class="profile-skills">
-        ${Avatar3d()}
-      </div>
+      ${Avatar3d()}
     </div>
   `;
 };
