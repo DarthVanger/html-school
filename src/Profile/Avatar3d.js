6b4cf09b2b02064b31c4c082777dcf8b8f8a3a2d
Ivan Sergiienko <ivan@freedomfi.com>
2022-11-03 04:23:46 +0200

    Create custom camera

--- src/Profile/Avatar3d.js
+++ src/Profile/Avatar3d.js
@@ -32,35 +32,29 @@ export const Avatar3d = () => {
 
       let boxSize = 4;
       let boxPosition = {
-        y: 20,
+        y: 10,
         x: 0,
         z: -20,
       };
 
-      // This creates a basic Babylon Scene object (non-mesh)
       var scene = new BABYLON.Scene(engine);
       scene.clearColor = BABYLON.Color3.Black();
     
       // https://www.babylonjs-playground.com/#Y2XX5A
-       var camera = new BABYLON.FollowCamera("camera", new BABYLON.Vector3(0, 0, 0), scene);
+      var camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(0, 0, 0), scene);
+      const cameraRadius = 20;
 
-      // The goal distance of camera from target
-      camera.radius = boxSize + 10;
-      // The goal height of camera above local origin (centre) of target
-      camera.heightOffset = 10;
-      //Acceleration of camera in moving from current to goal position
-      camera.cameraAcceleration = 0.05;
-      //The speed at which acceleration is halted 
-      camera.maxCameraSpeed = 10;
-      // This attaches the camera to the canvas
+      camera.rotation.x = 0;
+      camera.rotation.y = 0;
+      camera.rotation.z = 0;
+
       camera.attachControl(canvas, true);
 
-      // turn off camera collisions
       camera.checkCollisions=false;
 
       //camera.fov = Math.PI / 2;
 
-       // Add a light
+      // Add a light
       var light1 = new BABYLON.PointLight("light1", new BABYLON.Vector3(0,4,0), scene);
       light1.diffuse = new BABYLON.Color3(1, 0.4, 0.4);
       light1.specular = new BABYLON.Color3(0.5, 0.2, 0.2);
@@ -71,51 +65,16 @@ export const Avatar3d = () => {
 
       light.intensity = 0.7;
     
+      const wallSize = 100;
+      const wallNorthPosition = {
+        z: -103,
+        x: -4,
+        y: wallSize / 2,
+      };
 
-    // gravity
-    scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
-
-    // no gravity for camera
-    camera.applyGravity = false;
-    
-    // camera elipsoid
-    camera.ellipsoid = new BABYLON.Vector3(0.2, 0.2, 0.2);
-
-    const wallSize = 100;
-    const wallNorthPosition = {
-      z: -103,
-      x: -4,
-      y: wallSize / 2,
-    };
-
-   // walls
-//    var wallNorth=BABYLON.MeshBuilder.CreatePlane("wallNorth", {width: wallSize, height: wallSize, subdivsions: 1}, scene);
-//    wallNorth.position.y = wallNorthPosition.y;
-//    wallNorth.position.x = wallNorthPosition.x;
-//    wallNorth.position.z = wallNorthPosition.z;
-//    //wallNorth.addRotation(0, Math.PI, 0);
-//    //wallNorth.material=groundMaterial;
-//    wallNorth.checkCollisions=true;
-
-    //var wallSouth=BABYLON.MeshBuilder.CreatePlane("wallSouth", {width: 16, height: 2.7, subdivsions: 1}, scene);
-    //wallSouth.position.y=1.35;
-    //wallSouth.position.x=8;
-    //wallSouth.position.z=0;
-    //wallSouth.addRotation(0, Math.PI/2, 0);
-    //wallSouth.material=wall_mat;
-    //wallSouth.checkCollisions=true;
-    
       var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-      //groundMaterial.majorUnitFrequency = 5;
-      //groundMaterial.minorUnitVisibility = 0.45;
-      //groundMaterial.gridRatio = 2;
       groundMaterial.backFaceCulling = false;
-      //groundMaterial.mainColor = new BABYLON.Color3(1, 1, 1);
-      //groundMaterial.lineColor = new BABYLON.Color3(1.0, 1.0, 1.0);
-      //groundMaterial.opacity = 0.98;
-
       groundMaterial.diffuseTexture = new BABYLON.Texture("/src/Profile/weed.png");
-    
       var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "src/Profile/heightMap.png", 100, 82, 100, 0, 10, scene, false);
       ground.material = groundMaterial;
 
@@ -171,16 +130,6 @@ export const Avatar3d = () => {
 
       wall.material = wallMaterial;
   
-      //var skyMaterial = new BABYLON.GridMaterial("skyMaterial", scene);
-      //skyMaterial.majorUnitFrequency = 6;
-      //skyMaterial.minorUnitVisibility = 0.43;
-      //skyMaterial.gridRatio = 0.5;
-      //skyMaterial.mainColor = new BABYLON.Color3(0, 0.05, 0.2);
-      //skyMaterial.lineColor = new BABYLON.Color3(0, 1.0, 1.0);	
-      //skyMaterial.backFaceCulling = false;
-
-      //var skySphere = BABYLON.Mesh.CreateSphere("skySphere", 90, 330, scene);
-      //skySphere.material = skyMaterial;
 
       BABYLON.SceneLoader.ImportMesh("", "/src/Profile/Matilda/", "scene.gltf", scene, function (meshes) {
         const scene = meshes[0];
@@ -189,7 +138,6 @@ export const Avatar3d = () => {
       });
 
       let bong;
-
 
       let bongPosition = {
         y: 18,
@@ -220,7 +168,12 @@ export const Avatar3d = () => {
       box.position.y = boxPosition.y;
       box.position.x = boxPosition.x;
       box.position.z = boxPosition.z; 
-      box.rotation.y = Math.PI;
+      box.rotation.y = 0;
+      box.rotation.x = 0;
+      box.rotation.z = 0;
+      camera.position.y = boxPosition.y;
+      camera.position.x = boxPosition.x;
+      camera.position.z = boxPosition.z;
 
       var boxMaterial = new BABYLON.StandardMaterial("mat", scene);
 boxMaterial.backFaceCulling = true;
@@ -233,10 +186,9 @@ boxMaterial.backFaceCulling = true;
 
       let isBongMovingUp = true;
       engine.runRenderLoop(function () {
-        //camera.alpha += 0.001;
         if (bong) {
 
-          //box.rotation.y += 0.003;
+          box.rotation.y += 0.003;
           if (isBongMovingUp && bong.position.y >= bongPosition.y + 3) {
             isBongMovingUp = false;
           }
@@ -253,17 +205,6 @@ boxMaterial.backFaceCulling = true;
         }
       });	
 
-
-   var playerMesh=[];
-
-    // Player "character" version 1. They're a bit boxy, but it works.
-    var playerMesh = BABYLON.Mesh.CreateBox("playerMesh", 5.0, scene);
-    playerMesh.position.x=0;
-    playerMesh.position.z=0;
-    playerMesh.position.y=1;
-    //playerMesh.checkCollisions=true;
-    //camera.lockedTarget = playerMesh;
-    camera.lockedTarget = box;
 
     // WASD control of Player "character".
     let isWPressed = false;
@@ -294,10 +235,10 @@ boxMaterial.backFaceCulling = true;
       scene.registerBeforeRender (() => {
         if (!scene.isReady()){return;}
 
-        const phi = box.rotation.y;
-        const theta = box.rotation.x - Math.PI  / 2;
-        var playerSpeed=0.2;
-        const r =  playerSpeed;
+        const phi = camera.rotation.y;
+        const theta = camera.rotation.x + Math.PI / 2;
+        var playerSpeed = 0.5;
+        const r = playerSpeed;
 
         var x = r*parseFloat(Math.sin(phi) * Math.sin(theta));
         var z = r*parseFloat(Math.cos(phi) * Math.sin(theta));
@@ -306,34 +247,45 @@ boxMaterial.backFaceCulling = true;
         if (isWPressed || isSPressed) {
 
           if (isWPressed==true) {
-            //box.locallyTranslate(new BABYLON.Vector3(0, 0, 0.1));
             var forwards = new BABYLON.Vector3(x, y, z);
             box.moveWithCollisions(forwards);
           }
 
           if (isSPressed==true) {
-            //box.locallyTranslate(new BABYLON.Vector3(0, 0, -0.1));
-            var backwards = new BABYLON.Vector3(-x, y, -z);
+            var backwards = new BABYLON.Vector3(-x, -y, -z);
             box.moveWithCollisions(backwards);
           }
         }
 
         if (isQPressed==true) {
-          box.addRotation(-0.05,0,0);
+          box.addRotation(-0.01,0,0);
+          camera.rotation.x -= 0.05;
         }
 
         if (isEPressed==true) {
-          box.addRotation(0.05, 0, 0);
+          box.addRotation(0.01, 0, 0);
+          camera.rotation.x += 0.05;
         }
 
         if (isAPressed==true) {
-          box.addRotation(0,-0.05,0);
+          box.addRotation(0,-0.01,0);
+          camera.rotation.y -= 0.05;
         }
 
         if (isDPressed==true) {
-          box.addRotation(0,0.05,0);
+          box.addRotation(0,0.01,0);
+          camera.rotation.y += 0.05;
         }
 
+        const boxR = Math.hypot(box.position.x, box.position.y, box.position.z);
+        const camR = boxR + cameraRadius;
+        var camX = box.position.x - cameraRadius*parseFloat(Math.sin(phi) * Math.sin(theta));
+        var camZ = box.position.z - cameraRadius*parseFloat(Math.cos(phi) * Math.sin(theta));
+        var camY = box.position.y - cameraRadius*parseFloat(Math.cos(theta));
+
+        camera.position.x = camX;
+        camera.position.y = camY;
+        camera.position.z = camZ;
       });
 
       return scene;
