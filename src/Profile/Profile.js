1a3e821268e35a74aa5b2ead29a918c15e1608b7
Ivan Sergiienko <ivan@freedomfi.com>
2022-11-04 08:04:52 +0200

    Add level to the Profile svg

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,21 +1,66 @@
 import { cyberProfile } from './cyberProfile.js';
 
-export const Profile = () => {
-  const height = 100;
-  const width = 100;
+export const Profile = (state) => {
+  console.log('state: ', state);
+  const scale = 2;
+  const height = 296;
+  const width = 526;
   const x = 0;
   const y = 0;
+  const fontSize = 5;
+
+  const cx = width / 2;
+  const cy = height / 2;
+
+  const levelText = {
+    x: cx,
+    y: cy + 25,
+  }
+
+  const levelNumberText = {
+    x: cx,
+    y: cy - 18,
+  }
+
+  const getLevelNumberText = () => {
+    return document.querySelector('#level-number-text');
+  };
+
+  fetch('/tree')
+    .then(r => r.json())
+    .then(r => {
+      state.skills = r.skills;
+      state.levels = r.levels;
+      state.points = r.points;
+      state.categoryLevels = r.categoryLevels;
+      state.lecturePoints = r.lecturePoints;
+      state.questPoints = r.questPoints;
+      const level = state.levels[state.student];
+      console.log('111');
+      getLevelNumberText().innerHTML = level;
+    });
 
   return `
     <div id="profile">
-      <svg viewPort="0 0 ${width} ${height}">
+      <!--
+      <img src="/src/Profile/cyber-skills.jpg" />
+      -->
+      <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
         <image
           href="${cyberProfile}"
-          height="100%"
-          width="100%"
+          width="${width}"
+          height="${height}"
           x="${x}"
           y="${y}"
+          preserveAspectRatio="none"
         />
+        <text dominant-baseline="middle" text-anchor="middle" x="${levelText.x}" y="${levelText.y}" style="font-size: ${fontSize}">
+        Level
+        </text>
+
+        <text dominant-baseline="middle" text-anchor="middle" id="level-number-text" x="${levelNumberText.x}" y="${levelNumberText.y}" style="font-size: ${fontSize}">
+          0
+        </text>
       </svg>
     </div>
   `;
