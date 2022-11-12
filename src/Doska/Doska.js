e6a48a33a60bec9ab169eea58693b87c39b41f9c
Ivan Sergiienko <ivan@freedomfi.com>
2022-11-12 06:57:12 +0200

    Add Doska Pometa :)

---
+++ src/Doska/Doska.js
@@ -0,0 +1,89 @@
+const element = document.createElement('div');
+element.id = 'doska-pometa';
+
+export const Doska = (state) => {
+  const getElement = () => document.querySelector('#doska-pometa');
+
+  let isExpanded = false;
+
+  const expand = () => {
+    if (!isExpanded) {
+      getElement().className = 'expand';
+      isExpanded = true;
+    } else {
+      getElement().className = '';
+      isExpanded = false;
+    }
+  }
+
+  const renderWhenReady = () => {
+    if (!state.questPoints) {
+      setTimeout(render, 500);
+      return;
+    }
+  }
+
+  const render = () => {
+    const homeworks = state.questPoints;
+    console.log('homeworks: ', homeworks);
+    const now = new Date();
+    let lastWeekHomeworks = [];
+    for (let student in homeworks) {
+      for (let hw of homeworks[student]) {
+        const d = hw.date;
+        const date = new Date(d)
+        const now = new Date();
+        const diff = now.getTime() - date.getTime();
+        const diffMinutes = diff / 1000 / 60;
+
+        const diffDays = diffMinutes / 60 / 24;
+
+        if (diffDays < 7) {
+          lastWeekHomeworks.push({
+            student,
+            homework: hw,
+          });
+        }
+      }
+    };
+
+    const formatDate = (d) => {
+      const date = new Date(d);
+      var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+      return date.toLocaleDateString("uk-UA", options); // Saturday, September 17, 2016
+    }
+
+    const homeworkListHtml = lastWeekHomeworks.reverse().map(({ student, homework }) => `
+      <div class="entry">
+        <div class="left">
+          <img src="/img/${student}.jpg" />
+          <div>${student}</div>
+        </div>
+        <div class="right">
+          <p>${homework.id} - ${formatDate(homework.date)}</p>
+        </div>
+      </div>
+    `).join('');;
+
+    getElement().innerHTML = `
+      <button type="button" class="expand">ДОСКА ПОМËТА</button>
+      ${homeworkListHtml}
+    `;
+
+    getElement().querySelector('.expand').addEventListener('click', expand);
+
+    console.log('homeworks: ', homeworks);
+    console.log('lastWeekHomeworks: ', lastWeekHomeworks);
+  }
+
+  renderWhenReady();
+
+
+  element.innerHTML = `
+    <button type="button">Loading...</button>
+  `;
+
+  return element.outerHTML;
+
+}
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -2,116 +2,122 @@ import { BottomBar } from '../BottomBar.js';
 import { Quest } from '../quests/MainPage/Levels/Quest.js';
 import quests from '../quests/quests/quests.js';
 import { Profile } from '../Profile/Profile.js';
+import { Doska } from '../Doska/Doska.js';
 
-export const HomePage = (state) => `
-  <div id="home-page">
-    <div class="weed-border-left">
-      <img src="/src/HomePage/img/logo_leftt.png" />
+export const HomePage = (state) => {
+
+  return `
+    <div id="home-page">
+      <div class="weed-border-left">
+        <img src="/src/HomePage/img/logo_leftt.png" />
+      </div>
+
+      <div class="weed-border-top-center"></div>
+
+      <div class="weed-border-top-left">
+        <img src="/src/HomePage/img/logo_left.png" />
+      </div>
+
+      <div class="weed-border-top-right"></div>
+
+      <div class="weed-border-right">
+      </div>
+
+      <div align="justify" class="page-content">
+        <marquee>
+          <h1>ХТМЛ ШКОЛА - НАПАЛЕОН</h1>
+        </marquee>
+
+        ${Doska(state)}
+
+        ${Profile(state)}
+
+        <section id="main-page-links">
+          <article>
+            <h3>ДРЕВО УМЕНИЙ</h3>
+            <a href="#/skills">
+              <img src="src/HomePage/skills-tree.gif" />
+            </a>
+          </article>
+          <article>
+            <h3>ЗОДАНЕЯ</h3>
+            <a href="#/quests">
+              <img src="src/HomePage/lessons.gif" />
+            </a>
+          </article>
+          <article>
+            <h3>ПРОГРЕС</h3>
+            <a href="#/progress">
+              <img src="src/LearningProgress/learning-progress.png" />
+            </a>
+          </article>
+          <article>
+            <h3>СЛОВАРЕК</h3>
+            <a href="https://docs.google.com/document/d/1J89zb6vTpfljXmxV2cEJx522XhNt-hmU/edit#" target="_blank">
+              <img src="/src/img/vocabulary.jpeg" />
+            </a>
+          </article>
+          <article>
+            <h3>КУРСАЧ</h3>
+            <a href="#/coursework">
+              <img src="/src/Coursework/coursework.jpeg" />
+            </a>
+          </article>
+          <article>
+            <h3>Делоем РЕЗЮМЕ</h3>
+            <a href="#/resume">
+              <img src="src/Resume/resume.png" />
+            </a>
+          </article>
+          <article>
+            <h3>EXPECTATION / REALITY SHOW</h3>
+            <a href="#/expectation-reality">
+              <img src="src/ExpectationReality/expectation-reality.jpeg" />
+            </a>
+          </article>
+          <article>
+            <h3>Neural ART УЧЕНЕКОВ</h3>
+            <a href="#/neural-art">
+              <img src="src/NeuralArt/neural-art.jpg" />
+            </a>
+          </article>
+          <!--
+          <article>
+            <h3>ТВИЧ</h3>
+            <a href="#/twitch">
+              <img src="src/Twitch/twitch.png" />
+            </a>
+          </article>
+          -->
+          <!--
+          <article>
+            <h3>ЭКЗАМЕНЫ</h3>
+            <a href="#/quiz">
+              <img src="src/Quiz/quiz.jpg" />
+            </a>
+          </article>
+          -->
+          <!--
+          <article>
+            <h3>КУРСАЧИ</h3>
+            <a href="#/coursework">
+              <img src="src/Courserwork/courserwork.jpeg" />
+            </a>
+          </article>
+          -->
+        </section>
+      </div>
+
+      <div class="weed-border-left-bottom">
+      </div>
+
+      <div class="weed-border-bottom-center">
+      </div>
+
+      <div class="weed-border-right-bottom">
+      </div>
+
+      ${BottomBar(state)}
     </div>
-
-    <div class="weed-border-top-center"></div>
-
-    <div class="weed-border-top-left">
-      <img src="/src/HomePage/img/logo_left.png" />
-    </div>
-
-    <div class="weed-border-top-right"></div>
-
-    <div class="weed-border-right">
-    </div>
-
-    <div align="justify" class="page-content">
-      <marquee>
-        <h1>ХТМЛ ШКОЛА - НАПАЛЕОН</h1>
-      </marquee>
-
-      ${Profile(state)}
-
-      <section id="main-page-links">
-        <article>
-          <h3>ДРЕВО УМЕНИЙ</h3>
-          <a href="#/skills">
-            <img src="src/HomePage/skills-tree.gif" />
-          </a>
-        </article>
-        <article>
-          <h3>ЗОДАНЕЯ</h3>
-          <a href="#/quests">
-            <img src="src/HomePage/lessons.gif" />
-          </a>
-        </article>
-        <article>
-          <h3>ПРОГРЕС</h3>
-          <a href="#/progress">
-            <img src="src/LearningProgress/learning-progress.png" />
-          </a>
-        </article>
-        <article>
-          <h3>СЛОВАРЕК</h3>
-          <a href="https://docs.google.com/document/d/1J89zb6vTpfljXmxV2cEJx522XhNt-hmU/edit#" target="_blank">
-            <img src="/src/img/vocabulary.jpeg" />
-          </a>
-        </article>
-        <article>
-          <h3>КУРСАЧ</h3>
-          <a href="#/coursework">
-            <img src="/src/Coursework/coursework.jpeg" />
-          </a>
-        </article>
-        <article>
-          <h3>Делоем РЕЗЮМЕ</h3>
-          <a href="#/resume">
-            <img src="src/Resume/resume.png" />
-          </a>
-        </article>
-        <article>
-          <h3>EXPECTATION / REALITY SHOW</h3>
-          <a href="#/expectation-reality">
-            <img src="src/ExpectationReality/expectation-reality.jpeg" />
-          </a>
-        </article>
-        <article>
-          <h3>Neural ART УЧЕНЕКОВ</h3>
-          <a href="#/neural-art">
-            <img src="src/NeuralArt/neural-art.jpg" />
-          </a>
-        </article>
-        <!--
-        <article>
-          <h3>ТВИЧ</h3>
-          <a href="#/twitch">
-            <img src="src/Twitch/twitch.png" />
-          </a>
-        </article>
-        -->
-        <!--
-        <article>
-          <h3>ЭКЗАМЕНЫ</h3>
-          <a href="#/quiz">
-            <img src="src/Quiz/quiz.jpg" />
-          </a>
-        </article>
-        -->
-        <!--
-        <article>
-          <h3>КУРСАЧИ</h3>
-          <a href="#/coursework">
-            <img src="src/Courserwork/courserwork.jpeg" />
-          </a>
-        </article>
-        -->
-      </section>
-    </div>
-
-    <div class="weed-border-left-bottom">
-    </div>
-
-    <div class="weed-border-bottom-center">
-    </div>
-
-    <div class="weed-border-right-bottom">
-    </div>
-
-    ${BottomBar(state)}
-  </div>
-`;
+  `;
+}
