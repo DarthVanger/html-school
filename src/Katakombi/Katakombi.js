88f1ebff1044ba271f58cee8b92b6754cc6ccbec
Ivan Sergiienko <ivan@freedomfi.com>
2022-12-07 22:27:45 +0200

    Make catacombs move :)

--- src/Katakombi/Katakombi.js
+++ src/Katakombi/Katakombi.js
@@ -1,23 +1,52 @@
 const element = document.createElement('article');
+element.id = 'catacombs';
 export const Katakombi = () => {
+  const getSrc = i => `/src/Katakombi/img/catacombs-${i % 6 + 1}.jpeg`;
 
-  element.innerHTML = `
-    <h1>Katakombi</h1>
-    <div id="background">
-    </div>
-  `;
-
-  let images = [];
-  for (let i=1; i<=6; i++) {
-    let img = document.createElement('img');
-    let src = `/src/Katakombi/img/catacombs-${i}.jpeg`;
-
-    img.src = src;
-    images.push(img);
-    element.append(img);
+  const Level = (i) => {
+    const el = document.createElement('div');
+    el.className = 'level';
+    const img = document.createElement('img');
+    el.append(`Level ${i}`);
+    el.append(img);
+    img.src = getSrc(i);
+    return el;
   }
 
+  const levels = document.createElement('div');
+  levels.className = 'levels';
+  element.append(levels);
 
+  let images = [];
+  let curLevel = 1;
+  for (let i=1; i<=9; i++) {
+    const level = Level(i);
+    levels.append(level);
+    if (i == curLevel + 1) {
+      //level.className = 'cur';
+    }
+    images.push(level);
+  }
+
+  music();
+
+  function nextLevel() {
+    console.log('next level');
+    levels.classList.remove(`p-${curLevel}`);
+    curLevel++;
+    levels.classList.add(`p-${curLevel}`);
+  }
+
+  function getCurImg() {
+    return images[i];
+  }
+
+  document.body.addEventListener('click', nextLevel);
+
+  return element;
+}
+
+function music() {
   let isMusicPlaying = false;
   document.body.addEventListener('click', () => {
     if (!isMusicPlaying) {
@@ -26,6 +55,4 @@ export const Katakombi = () => {
       audio.play();
     }
   });
-
-  return element;
 }
