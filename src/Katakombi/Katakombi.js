b70f9df0249817778f0b28168dbfdf2100056bff
Ivan Sergiienko <ivan@freedomfi.com>
2022-12-08 00:11:38 +0200

    Refactor katakombi movement step 1

--- src/Katakombi/Katakombi.js
+++ src/Katakombi/Katakombi.js
@@ -17,31 +17,89 @@ export const Katakombi = () => {
   levels.className = 'levels';
   element.append(levels);
 
-  let images = [];
-  let curLevel = 1;
-  for (let i=1; i<=9; i++) {
+  const buttons = document.createElement('div');
+  buttons.className = 'buttons';
+
+  const btnUp = document.createElement('button');
+  btnUp.className = 'btn btn-up';
+  btnUp.innerHTML = 'up';
+  buttons.append(btnUp);
+  const btnDown = document.createElement('button');
+  btnDown.className = 'btn btn-down';
+  btnDown.innerHTML = 'down';
+  buttons.append(btnDown);
+  const btnRight = document.createElement('button');
+  btnRight.className = 'btn btn-right';
+  btnRight.innerHTML = 'right';
+  buttons.append(btnRight);
+  const btnLeft = document.createElement('button');
+  btnLeft.className = 'btn btn-left';
+  btnLeft.innerHTML = 'left';
+  buttons.append(btnLeft);
+
+  btnUp.addEventListener('click', moveUp);
+  btnDown.addEventListener('click', moveDown);
+  btnRight.addEventListener('click', moveRight);
+  btnLeft.addEventListener('click', moveLeft);
+
+
+  element.append(buttons);
+
+  const animTime = 2000;
+  let lx = 0;
+  let ly = 0;
+
+  function moveDown () {
+    let lcur = ly;
+    ly = (ly + 1) % 3;
+    levelElems[lcur].classList.add('cur-down');
+    levelElems[ly].classList.add('top-down');
+    levelElems[ly].classList.add('cur');
+    setTimeout(() => {
+      levelElems[lcur].classList.remove('cur');
+      levelElems[lcur].classList.remove('cur-down');
+      levelElems[ly].classList.remove('top-down');
+    }, animTime);
+  }
+
+  function moveUp () {
+    let lcur = ly;
+    ly = (ly + 1) % 3;
+    levelElems[ly].classList.add('bottom-up');
+    levelElems[ly].classList.add('cur');
+    levelElems[lcur].classList.add('cur-up');
+    setTimeout(() => {
+      levelElems[lcur].classList.remove('cur');
+      levelElems[lcur].classList.remove('cur-up');
+      levelElems[ly].classList.remove('bottom-up');
+    }, animTime);
+  }
+
+  function moveRight () {
+    let lcur = ly;
+    ly = (ly + 1) % 3;
+    levelElems[ly].classList.add('left-right');
+    levelElems[lcur].classList.add('cur-right');
+  }
+
+  function moveLeft () {
+    let lcur = ly;
+    ly = (ly + 1) % 3;
+    levelElems[ly].classList.add('righ-left');
+    levelElems[lcur].classList.add('cur-left');
+  }
+
+  let levelElems = [];
+  for (let i=0; i<9; i++) {
     const level = Level(i);
     levels.append(level);
-    if (i == curLevel + 1) {
-      //level.className = 'cur';
+    if (i == 0) {
+      level.classList.add('cur');
     }
-    images.push(level);
+    levelElems.push(level);
   }
 
   music();
-
-  function nextLevel() {
-    console.log('next level');
-    levels.classList.remove(`p-${curLevel}`);
-    curLevel++;
-    levels.classList.add(`p-${curLevel}`);
-  }
-
-  function getCurImg() {
-    return images[i];
-  }
-
-  document.body.addEventListener('click', nextLevel);
 
   return element;
 }
