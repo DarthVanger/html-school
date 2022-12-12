3b822f4f5e8b700f73b5a9539bde5e3042d74d68
Ivan Sergiienko <ivan@freedomfi.com>
2022-12-12 15:57:32 +0200

    Add lecture stats for new students

--- server/db/lectureStats.js
+++ server/db/lectureStats.js
@@ -1,12 +1,14 @@
 import { lectures } from '../../src/LearningProgress/lectures.js';
 
-export const lectureStats = lectures.map(lecture => {
+export const lectureStats = lectures.map((lecture, idx) => {
+  const from = i => idx >= i ? 1 : 0;
   return {
     'other-species': 0,
-    dimon: 1,
-    johnny: 1,
-    tony: 1,
-    insane: 0,
-    russi4: 0,
+    dimon: from(0),
+    johnny: from(0),
+    tony: from(0),
+    'mister-smith': from(20),
+    valik_h: from(22),
+    mazerTim: from(26),
   };
 });
