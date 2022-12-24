3e3be8b1e41955ddb483ddfecd67549ce581cea7
Ivan Sergiienko <ivan@freedomfi.com>
2022-12-24 12:04:01 +0200

    SlidesV2: improv main page styles

--- src/SlidesV2/lessons/Lesson28.js
+++ src/SlidesV2/lessons/Lesson28.js
@@ -7,64 +7,67 @@ export const Lesson28 = () => {
   let curSection;
   let curSlideIdx;
   el.innerHTML = `
-    <section class="pomodoro">
-      <h2>Objects</h2>
+    <h1>Lesson #28</h1>
+    <div class="lesson-sections">
+      <section class="pomodoro">
+        <h2>Objects</h2>
 
-      <article>
-        <h1>Уроке #28</h1>
-        <section class="content">
-          <h2>Погенали )</h2>
-        </section>
-      </article>
+        <article>
+          <h1>Уроке #28</h1>
+          <section class="content">
+            <h2>Погенали )</h2>
+          </section>
+        </article>
 
-      <article>
-        <section class="content">
-          <figure>
-            <img src="/img/lesson28/za-vstre4y.jpeg" />
-          </figure>
-          <figcaption>
-            За ВстречУ! За УроК!
-          </figcaption>
-        </section>
-      </article>
+        <article>
+          <section class="content">
+            <figure>
+              <img src="/img/lesson28/za-vstre4y.jpeg" />
+            </figure>
+            <figcaption>
+              За ВстречУ! За УроК!
+            </figcaption>
+          </section>
+        </article>
 
-      <article>
-        ${Lety4ka()}
-      </article>
+        <article>
+          ${Lety4ka()}
+        </article>
 
-      <article data-timer="5">
-        <section class="content">
-          <ol>
-            <li>Create two variables: x and y, and assign any numbers to them.</li>
-            <li>Print text "x:" on the screen</li>
-            <li>Print the value of variable x on the screen</li>
-            <li>Print "&lt;br /&gt;" on the screen</li>
-            <li>Print text "y:" on the screen</li>
-            <li>Print the value of variable y on the screen</li>
-          </ol>
-        </section>
-      </article>
-    </section>
+        <article data-timer="5">
+          <section class="content">
+            <ol>
+              <li>Create two variables: x and y, and assign any numbers to them.</li>
+              <li>Print text "x:" on the screen</li>
+              <li>Print the value of variable x on the screen</li>
+              <li>Print "&lt;br /&gt;" on the screen</li>
+              <li>Print text "y:" on the screen</li>
+              <li>Print the value of variable y on the screen</li>
+            </ol>
+          </section>
+        </article>
+      </section>
 
-    <section class="pomodoro">
-      <h2>Codeacedemy</h2>
-    </section>
+      <section class="pomodoro">
+        <h2>Codeacedemy</h2>
+      </section>
 
-    <section class="pomodoro">
-      <h2>Game #1</h2>
-    </section>
+      <section class="pomodoro">
+        <h2>Game #1</h2>
+      </section>
 
-    <section class="pomodoro">
-      <h2>Game #2</h2>
-    </section>
+      <section class="pomodoro">
+        <h2>Game #2</h2>
+      </section>
 
-    <section class="pomodoro">
-      <h2>Unity</h2>
-    </section>
-    
-    <section class="pomodoro">
-      <h2>Feedback</h2>
-    </section>
+      <section class="pomodoro">
+        <h2>Unity</h2>
+      </section>
+      
+      <section class="pomodoro">
+        <h2>Feedback</h2>
+      </section>
+    </div>
   `;
 
   const sections = el.querySelectorAll('section.pomodoro');
@@ -98,8 +101,11 @@ export const Lesson28 = () => {
     sections.forEach(section => section.style.display = 'none');
     sectionEl.style.display = 'block';
     console.log('section El: ', sectionEl);
+    const h1 = el.querySelector('h1');
+    h1.style.display = 'none';
     const h2 = sectionEl.querySelector('h2');
     h2.style.display = 'none';
+    el.querySelector('.lesson-sections').classList.remove('lesson-sections');
     renderCurSlide();
   }
 
