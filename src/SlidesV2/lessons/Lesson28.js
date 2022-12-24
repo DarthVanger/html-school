92477c61db34caa807c33cb344882f0468cd7248
Ivan Sergiienko <ivan@freedomfi.com>
2022-12-24 15:46:36 +0200

    Addd more slides to lesson28

--- src/SlidesV2/lessons/Lesson28.js
+++ src/SlidesV2/lessons/Lesson28.js
@@ -39,22 +39,130 @@ export const Lesson28 = () => {
             </ol>
           </section>
         </article>
+
+        <article data-timer="5">
+          <section class="content">
+            <ol>
+              <li>Create object named "point", which has two properties: x and y. Assign any numbers to point.x and point.y</li>
+              <li>Print text "point.x" on the screen</li>
+              <li>Print the value of point.x on the screen</li>
+              <li>Print "<br />" on the screen</li>
+              <li>Print text "point.y:" on the screen</li>
+              <li>Print the value of point.y on the screen</li>
+            </ol>
+          </section>
+        </article>
+
+        <article data-timer="5">
+          <h2>ОНАЛЕЗ</h2>
+          <section class="content">
+            <p>
+              Сравниваем коды.
+            </p>
+            <p>
+              В чем разница?
+            </p>
+          </section>
+        </article>
+
+        <article data-timer="5">
+          <h2>Добавляем height, width</h2>
+          <section class="content">
+            <p>
+              Добавляем в первое зоданее height, width.
+            </p>
+            <p>
+              Во второе задание добавляем point.height, point.width.
+            </p>
+          </section>
+        </article>
+
+        <article data-timer="5">
+          <h2>Добавляем вторую точку</h2>
+          <section class="content">
+            <p>
+              Добавляем в первое зоданее еще 4 переенных для второй точки.
+            </p>
+            <p>
+              Во второе задание добавляем обьект point2, с координатами и розмером.
+            </p>
+          </section>
+        </article>
+
+        <article data-timer="5">
+          <h2>ОНАЛЕЗ</h2>
+          <section class="content">
+            <p>
+              Сравниваем коды.
+            </p>
+            <p>
+              В чем разница?
+            </p>
+          </section>
+        </article>
+
+        <article>
+          <section class="content">
+            <p>
+              Все нахуй короч)
+            </p>
+          </section>
+        </article>
       </section>
 
       <section class="pomodoro">
-        <h2>Codeacedemy</h2>
+        <article>
+          <h2>Codeacedemy</h2>
+          <section class="content">
+            <p>
+              Go домашку :)
+            </p>
+          </section>
+        </article>
       </section>
 
       <section class="pomodoro">
         <h2>Game #1</h2>
+        <article>
+          <section class="content">
+            <p>
+              Игра го)
+            </p>
+            <p>
+              renderEnemy делает 1 функцию вместо 3
+            </p>
+          </section>
+        </article>
       </section>
 
       <section class="pomodoro">
         <h2>Game #2</h2>
+        <article>
+          <section class="content">
+            <p>
+              Го погнали шо-то делоем))
+            </p>
+          </section>
+        </article>
       </section>
 
       <section class="pomodoro">
         <h2>Unity</h2>
+        <figure>
+          <video>
+            <source src="/video/gamedev/unity-editor-overview.mp4" type="video/mp4" />
+          </video>
+          <figcaption>
+            За ВстречУ! За УроК!
+          </figcaption>
+        </figure>
+        <article>
+          <section class="content">
+            <p>
+              Го погнали шо-то делоем))
+            </p>
+          </section>
+        </article>
       </section>
       
       <section class="pomodoro">
@@ -113,6 +221,11 @@ export const Lesson28 = () => {
 
   function nextSlide() {
     curSlideIdx++;
+    renderCurSlide();
+  }
+
+  function prevSlide() {
+    curSlideIdx--;
     renderCurSlide();
   }
 
