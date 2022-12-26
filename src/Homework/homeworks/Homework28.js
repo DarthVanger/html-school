8ad734b72c635f192b70887c2555e7ed23f867cc
Ivan Sergiienko <ivan@freedomfi.com>
2022-12-26 21:52:33 +0200

    Update Homework28: img and let choose any task

--- src/Homework/homeworks/Homework28.js
+++ src/Homework/homeworks/Homework28.js
@@ -5,6 +5,9 @@ export const Homework28 = () => {
       <p>Делоем хотя бы адЫн Лессон Модуля <b><a href="https://www.codecademy.com/learn/introduction-to-javascript">Джаваскрипт Интродюсион</a></b>.</p>
       <p>А именно - самый первый - <b><a href="https://www.codecademy.com/courses/introduction-to-javascript/lessons/introduction-to-javascript">Introduction to Javascript</a></b>.</p>
       <p>Ниже инструксийон, со скриншотион :)</p>
+      <p>
+        <b>Кто Хочет - делает любой другой модуль \ урок CodeAcedemy, на любом языке программирования</b>
+      </p>
       <figure>
         <img src="/img/homework/hw28-1.png" />
         <figcaption>
