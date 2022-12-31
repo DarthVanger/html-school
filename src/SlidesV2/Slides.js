24431e067ee5e1b3844a957e4cf61de78d51dc11
Ivan Sergiienko <ivan@freedomfi.com>
2022-12-31 02:56:12 +0200

    Add New Year lesson :)

--- src/SlidesV2/Slides.js
+++ src/SlidesV2/Slides.js
@@ -1,9 +1,11 @@
 import { Lesson28 } from './lessons/Lesson28.js';
+import { LessonNewYear } from './lessons/LessonNewYear.js';
 export const Slides = ({lessonName}) => {
   const el = document.createElement('div');
   el.className = "slides";
   const lessons = {
     lesson28: Lesson28(),
+    lessonnewyear: LessonNewYear(),
   };
   console.log('lessonname: ', lessonName);
   el.append(lessons[lessonName]);
---
+++ src/SlidesV2/lessons/LessonNewYear.js
@@ -0,0 +1,223 @@
+import { Timer } from './Timer.js';
+import { Lety4ka } from './Lety4ka.js';
+import { Lesson } from './Lesson.js';
+
+const html = `
+  <h1>Lesson #New #Year #2023 ** !!</h1>
+  <div class="lesson-sections">
+    <section class="pomodoro">
+      <h2>С Новым Годом!!</h2>
+
+      <article>
+        <section class="content">
+          <figure>
+            <img src="/img/lesson28/za-vstre4y.jpeg" />
+            <figcaption>
+              За ВстречУ! За УроК! За Новый Год!
+            </figcaption>
+          </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <p>Что приозошло за этот год?</p>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <h2>
+            HTML ШКОЛА
+          </h2>
+          <figure>
+            <img src="/src/img/school.jpeg" />
+            <figcaption>
+              НА ВЕНИКЕ
+            </figcaption>
+          </figure>
+        </section>
+      </article>
+
+
+      <article>
+        <section class="content">
+          <figure>
+            <img src="/img/lesson28/za-vstre4y.jpeg" />
+            <figcaption>
+              Еще по одной??
+            </figcaption>
+          </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+        <h2>
+          МОТИВАЦИЯ
+        </h2>
+        <figure>
+          <img src="/src/img/it-cloud.png" />
+          <figcaption>
+            ТВОРЧЕСТВО, СОЗДАНИЕ ХУЙНИ
+          </figcaption>
+        </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <h2>Всем спасибо - План хороший!</h2>
+        </section>
+      </article>
+
+
+      <article>
+        <section class="content">
+        <h2>
+          НА ОСТРИЕ РАЗВИТИЯ
+        </h2>
+        <figure>
+          <img src="/src/img/future.jpeg" />
+          <figcaption>
+            БУДУЩЕЕ ЗА ЭТИМ ДЕРЬМОМ - ДРОНЫ, ХУЕНЫ
+          </figcaption>
+        </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <h2>
+            ЧТО ДЛЯ ЭТОГО НУЖНО
+          </h2>
+          <figure>
+            <img src="/src/img/dro4ka.jpeg" />
+            <figcaption>
+              ЗАДРОТСВО
+            </figcaption>
+          </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <figure>
+            <img src="/src/img/courses.png" />
+          </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <figure>
+            <img src="/src/img/cplusplus.jpg" />
+          </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <figure>
+            <img src="/src/img/cplusplus-2.jpg" />
+          </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <figure>
+            <img src="/img/lesson28/za-vstre4y.jpeg" />
+            <figcaption>
+              Еще по одной??
+            </figcaption>
+          </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <h2>Expectation / Reality</h2>
+          <p>
+            <a href="/#/expectation-reality">ССЫЛКЕ</a>
+          </p>
+          <p>
+            МЫСЛЕ
+          </p>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <p>
+            Все нахуй короч)
+          </p>
+        </section>
+      </article>
+    </section>
+
+    <section class="pomodoro">
+      <h2>Joystick</h2>
+      <article>
+        <section class="content">
+          <p>
+            Палочка удовольстивия
+          </p>
+        </section>
+      </article>
+    </section>
+
+    <section class="pomodoro">
+      <h2>English speaking club</h2>
+      <article>
+        <section class="content">
+          <figure>
+            <video>
+              <source src="/video/carol-nato.mp4" type="video/mp4" />
+            </video>
+          </figure>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+          <h2>Discussion</h2>
+          <p>So this is are our allies... NATO POWER...</p>
+          <p>Thoughts? :)</p>
+        </section>
+      </article>
+
+      <article>
+        <section class="content">
+            <img src="/img/lesson-new-year/new-years-resolutions.jpeg" />
+            <figcaption>
+              New Year's Resolutions
+            </figcaption>
+          </figure>
+        </section>
+      </article>
+    </section>
+
+    <section class="pomodoro">
+      <h2>Joystick #2</h2>
+      <article>
+        <section class="content">
+          <p>
+            Йо йо йо :)
+          </p>
+        </section>
+      </article>
+    </section>
+
+    <section class="pomodoro">
+      <h2>Feedback</h2>
+      <article>
+        <section class="content">
+          <p>Фидек за занятие и годд</p>
+        </section>
+      </article>
+    </section>
+  </div>
+`;
+
+export const LessonNewYear = () => Lesson({ html });
