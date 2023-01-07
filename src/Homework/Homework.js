23eb76f44e1e9f0551858e2ecab304085621d49e
Ivan Sergiienko <ivan@freedomfi.com>
2023-01-07 16:31:00 +0200

    Update Homework to #29

--- src/Homework/Homework.js
+++ src/Homework/Homework.js
@@ -1,10 +1,11 @@
 import { Homework28 } from './homeworks/Homework28.js';
+import { Homework29 } from './homeworks/Homework29.js';
 
 export const Homework = (state) => {
   return `
     <h2>ДОМАШЕКА</h2>
     <div id="homework">
-      ${Homework28(state)}
+      ${Homework29(state)}
     </div>
   `;
 };
---
+++ src/Homework/homeworks/Homework29.js
@@ -0,0 +1,9 @@
+export const Homework29 = () => {
+  return `
+    <div id="hw-28">
+      <p>CodeAcademy делоем :)</p>
+      <p>По 1 Лессону делаем хотя бы.</p>
+      <p>Тони - хотя бы с 0% сдвинуться ))</p>
+    </div>
+  `;
+}
