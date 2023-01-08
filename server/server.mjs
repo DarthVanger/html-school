44d19eaad5304b6f456d37a578141f35dde69985
Ivan Sergiienko <ivan@freedomfi.com>
2023-01-08 05:24:49 +0200

    Banki: improve a console log :)

--- server/server.mjs
+++ server/server.mjs
@@ -66,7 +66,7 @@ const runApp = async () => {
       }
     }
 
-    console.log('send banki:', db.data.banki);
+    console.log('WS: send banki');
     ws.send(JSON.stringify({
       name: 'banki',
       payload: db.data.banki,
