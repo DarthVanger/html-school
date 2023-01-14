b477ebd2c9c78b29989910c4d9e9a4adae7f7b2c
Ivan Sergiienko <ivan@freedomfi.com>
2023-01-14 21:00:15 +0200

    Add GeoGebra to HomePage :)

---
+++ src/GeoGebra/GeoGebra.js
@@ -0,0 +1,3 @@
+export const GeoGebra = () => `
+  <iframe src="https://www.geogebra.org/geometry/p8hw9dc4?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+`;
