71037404dc510b6a95ac09cbe270a29f8d4940a2
Ivan Sergiienko <ivan@freedomfi.com>
2023-01-14 21:59:54 +0200

    Add more GeoGebra to HomePage :)

--- src/GeoGebra/GeoGebra.js
+++ src/GeoGebra/GeoGebra.js
@@ -1,3 +1,14 @@
 export const GeoGebra = () => `
-  <iframe src="https://www.geogebra.org/geometry/p8hw9dc4?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+  <figure>
+    <h3>Circles - The best :))</h3>
+    <iframe src="https://www.geogebra.org/geometry/p8hw9dc4?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+  </figure>
+  <figure>
+    <h3>Circles - Johnny Edition</h3>
+    <iframe src="https://www.geogebra.org/geometry/umerztr3?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+  </figure>
+  <figure>
+    <h3>Steps - Napaleon Edition :D</h3>
+    <iframe src="https://www.geogebra.org/geometry/mt2hbpur?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+  </figure>
 `;
