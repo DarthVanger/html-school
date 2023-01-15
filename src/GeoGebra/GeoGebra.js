f9dfab5bad90604550cb3bca3d3741f55ac54739
Ivan Sergiienko <ivan@freedomfi.com>
2023-01-15 02:46:58 +0200

    Correct GeoGebra order/names

--- src/GeoGebra/GeoGebra.js
+++ src/GeoGebra/GeoGebra.js
@@ -1,7 +1,7 @@
 const geo = [
-  `<iframe src="https://www.geogebra.org/geometry/umerztr3?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
-`,
   `<iframe src="https://www.geogebra.org/geometry/p8hw9dc4?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+`,
+  `<iframe src="https://www.geogebra.org/geometry/umerztr3?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
 `,
   `<iframe src="https://www.geogebra.org/geometry/mt2hbpur?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
 `
