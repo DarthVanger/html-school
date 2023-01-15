a1ce90ee4e086323fe10b379f6c055d03190535b
Ivan Sergiienko <ivan@freedomfi.com>
2023-01-15 02:33:17 +0200

    Make GeoGeogebra shown on click

--- src/GeoGebra/GeoGebra.js
+++ src/GeoGebra/GeoGebra.js
@@ -1,14 +1,40 @@
-export const GeoGebra = () => `
+const geo = [
+  `<iframe src="https://www.geogebra.org/geometry/umerztr3?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+`,
+  `<iframe src="https://www.geogebra.org/geometry/p8hw9dc4?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+`,
+  `<iframe src="https://www.geogebra.org/geometry/mt2hbpur?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+`
+];
+
+export const GeoGebra = () => {
+  setTimeout(() => {
+    document.querySelectorAll('figure').forEach((fig, idx) => {
+      const btn = fig.querySelector('button');
+
+      //const i = fig.querySelector('iframe');
+      //i.style.height = '0vh';
+      //i.style.transition = 'all 2s';
+      btn.addEventListener('click', e => {
+        e.target.remove();
+        fig.innerHTML += geo[idx];
+      });
+    });
+  });
+
+  return `
+  <h2>GeoGebra</h2>
   <figure>
     <h3>Circles - The best :))</h3>
-    <iframe src="https://www.geogebra.org/geometry/p8hw9dc4?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+    <button>Show GeoGebra</button>
   </figure>
   <figure>
     <h3>Circles - Johnny Edition</h3>
-    <iframe src="https://www.geogebra.org/geometry/umerztr3?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+    <button>Show GeoGebra</button>
   </figure>
   <figure>
     <h3>Steps - Napaleon Edition :D</h3>
-    <iframe src="https://www.geogebra.org/geometry/mt2hbpur?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
+    <button>Show GeoGebra</button>
   </figure>
-`;
+  `;
+}
