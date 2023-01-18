26a8a975f172016b0fc6d5a57a2607bfe78d12cb
Ivan Sergiienko <ivan@freedomfi.com>
2023-01-18 23:51:40 +0200

    Homework31: add 2nd drone for gepard :)

--- gepard/app.js
+++ gepard/app.js
@@ -2,23 +2,22 @@ console.log('app.js xuyyyyyyyy');
 
 const fps = 1;
 const shahedVx = 10;
+const drone2Vx = 20;
 
 const shahed = document.querySelector('#shahed');
+const drone2 = document.querySelector('#drone2');
 const bullet = document.querySelector('#bullet');
 const circle = document.querySelector('#circle');
 
 let shahedX = 300;
+let drone2X = 300;
 let circleX = 0;
 let bulletX = 0;
 let bulletY = 0;
 let bulletWidth = 30;
 let bulletHeight = 30;
 
-
-// 10px per fps
-const Va = 10;
-
-// 50px per fps or
+// V = sqrt(Vx^2 + Vy^2) = 50
 const Vx = 30;
 const Vy = 40;
 
@@ -31,7 +30,7 @@ const point = {
   y: 400,
 };
 
-// Math.hypot = Sqrt(x^2 + y^2)
+// Math.hypot = sqrt(x^2 + y^2)
 const dlina = Math.hypot(point.x, point.y);
 const px = point.x / dlina;
 const py = point.y / dlina;
@@ -46,6 +45,7 @@ let stepNum = 0;
 function step() {
   stepNum +=1;
   shahedX += shahedVx;
+  drone2X += drone2Vx;
   circleX += Vx;
   bulletX += Vx;
   bulletY += Vy;
@@ -53,6 +53,7 @@ function step() {
   bullet.style.left = bulletX + 'px';
   bullet.style.top = bulletY + 'px';
   shahed.style.left = shahedX + 'px';
+  drone2.style.left = drone2X + 'px';
 
   // Calculate speed of the bullet
   const speed = Math.sqrt(Vx*Vx + Vy*Vy);
