a6fdaabea879cf00e73117f02731907f762cd8e1
DarthVanger <darthvanger@gmail.com>
2023-02-01 21:20:56 +0200

    Katakombi: Remove times2 lvl

--- src/Katakombi/levels/levels.js
+++ src/Katakombi/levels/levels.js
@@ -1,11 +1,9 @@
 import { minus2 } from './minus2.js';
-import { times2 } from './times2.js';
 import { sum } from './sum.js';
 import { distance } from './distance.js';
 
 export const levels = [
   minus2,
-  times2,
   sum,
   distance,
   //pointAfterSteps,
