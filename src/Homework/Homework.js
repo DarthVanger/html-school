901708f1a1aecab901ea6a5a8fdfe5ab22e5c8ea
DarthVanger <darthvanger@gmail.com>
2023-02-07 01:03:08 +0200

    Add Homework34
    And rename previous homework34 to homework33, as it should've been :)

--- src/Homework/Homework.js
+++ src/Homework/Homework.js
@@ -2,6 +2,7 @@ import { Homework28 } from './homeworks/Homework28.js';
 import { Homework29 } from './homeworks/Homework29.js';
 import { Homework31 } from './homeworks/Homework31.js';
 import { Homework32 } from './homeworks/Homework32.js';
+import { Homework33 } from './homeworks/Homework33.js';
 import { Homework34 } from './homeworks/Homework34.js';
 
 export const Homework = (state) => {
---
+++ src/Homework/homeworks/Homework33.js
@@ -0,0 +1,13 @@
+import { WeedSvg } from '../../WeedSvg.js';
+
+export const Homework33  = () => `
+<h2>ДОМАШЕКА #33</h2>
+
+<p>
+  Tony - CodeAcademy 1 лессон пройти.
+</p>
+
+<p>
+  Остальным - че хотите) игру делать или че-то еще.
+</p>
+`;
