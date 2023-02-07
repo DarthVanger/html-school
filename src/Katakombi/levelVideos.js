35f55dbefaa103cf5a0fe9f7ee8d178c2da05e2e
DarthVanger <darthvanger@gmail.com>
2023-02-07 22:51:42 +0200

    Katakombi: Update lawyer video: stop on blood :)

--- src/Katakombi/levelVideos.js
+++ src/Katakombi/levelVideos.js
@@ -3,7 +3,7 @@ export const levelVideos = [
   { src: '/video/katakombi/02-entrance-and-visioner-convulsions.mp4', duration: 39 },
   { src: '/video/katakombi/04-visioner-ghosts.mp4', duration: 32 },
   { src: '/video/katakombi/05-visioner-spit-and-lawyer-visit.mp4', duration: 31 },
-  { src: '/video/katakombi/06-lawyer-dies.mp4', duration: 74 },
+  { src: '/video/katakombi/06-lawyer-dies.mp4', duration: 72 },
   { src: '/video/katakombi/07-girl-bathroom-with-ghost.mp4', duration: 33 },
   { src: '/video/katakombi/08-girl-bathroom-im-sorry.mp4', duration: 98 },
 ];
