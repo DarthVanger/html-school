4c1aa1396c3946824f756ebf3c0d8966587b00ad
DarthVanger <darthvanger@gmail.com>
2023-02-09 02:17:07 +0200

    Katakombi: correct isPointInRectangleTask

--- src/Katakombi/levels/isPointInRectangle.js
+++ src/Katakombi/levels/isPointInRectangle.js
@@ -1,20 +1,33 @@
 const id = 'isPointInRectangle';
 const tests = (f) => [
   {
-    name: 'isPointInRectangle({ x: 0, y: 0 }, { x: -5, y: -5, w: 10, h: 10 }) returns true',
-    test: () => f({ x: 0, y: 0 }, { x: -5, y: -5, w: 10, h: 10 }) === true,
+    name: 'isPointInRectangle({ x: -4, y: 2 }, { x: -5, y: -5, w: 10, h: 10 }) returns true',
+    test: () => f({ x: -4, y: 2 }, { x: -5, y: -5, w: 10, h: 10 }) === true,
   },
   {
-    name: 'isPointInRectangle({ x: 15, y: 15 }, { x: -5, y: -5, w: 10, h: 10 }) returns false',
-    test: () => f({ x: 15, y: 15 }, { x: -5, y: -5, w: 10, h: 10 }) === false,
+    name: 'isPointInRectangle({ x: 4, y: 4 }, { x: -5, y: -5, w: 10, h: 10 }) returns true',
+    test: () => f({ x: 4, y: 4 }, { x: -5, y: -5, w: 10, h: 10 }) === true,
   },
   {
-    name: 'isPointInRectangle({ x: 0, y: 0 }, { x: -5, y: -5, w: 10, h: 10 }) returns true',
-    test: () => f({ x: 0, y: 0 }, { x: -5, y: -5, w: 10, h: 10 }) === true,
+    name: 'isPointInRectangle({ x: 2, y: -4 }, { x: -5, y: -5, w: 10, h: 10 }) returns true',
+    test: () => f({ x: -3, y: -3 }, { x: -5, y: -5, w: 10, h: 10 }) === true,
+  },
+
+  {
+    name: 'isPointInRectangle({ x: -4, y: 6 }, { x: -5, y: -5, w: 10, h: 10 }) returns false',
+    test: () => f({ x: -4, y: 6 }, { x: -5, y: -5, w: 10, h: 10 }) === false,
   },
   {
-    name: 'isPointInRectangle({ x: -6, y: 7 }, { x: -5, y: -5, w: 10, h: 10 }) returns false',
-    test: () => f({ x: -6, y: 7 }, { x: -5, y: -5, w: 10, h: 10 }) === false,
+    name: 'isPointInRectangle({ x: 6, y: 4 }, { x: -5, y: -5, w: 10, h: 10 }) returns false',
+    test: () => f({ x: 6, y: 4 }, { x: -5, y: -5, w: 10, h: 10 }) === false,
+  },
+  {
+    name: 'isPointInRectangle({ x: 8, y: -4 }, { x: -5, y: -5, w: 10, h: 10 }) returns false',
+    test: () => f({ x: 8, y: -4 }, { x: -5, y: -5, w: 10, h: 10 }) === false,
+  },
+  {
+    name: 'isPointInRectangle({ x: -2, y: -8 }, { x: -5, y: -5, w: 10, h: 10 }) returns false',
+    test: () => f({ x: -2, y: -8 }, { x: -5, y: -5, w: 10, h: 10 }) === false,
   },
 ];
 
