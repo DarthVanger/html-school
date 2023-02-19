99b0e51dd2c15def563096ab1b5901efdb968a5b
DarthVanger <darthvanger@gmail.com>
2023-02-19 21:20:54 +0200

    Update Lesson36

--- src/SlidesV2/lessons/Lesson36.js
+++ src/SlidesV2/lessons/Lesson36.js
@@ -3,5 +3,12 @@ import { Lesson } from './Lesson.js';
 import { Vangers } from '../../Vangers/Vangers.js';
 
 export const Lesson36 = (props) => {
-  return Vangers(props);
+  const element = document.createElement('section');
+  element.innerHTML = `
+    <h2>Уроке №36</h2>
+    <p>Создаем Мир вместе - <a href="#/vangers">Вангеры ссылке</a></p>
+  `;
+
+
+  return element;
 }
