dc2d486fba62f3aa6a832e762c4d546c4abbd739
DarthVanger <darthvanger@gmail.com>
2023-03-25 23:43:33 +0200

    Move GeoGebra into Homework/homeworks/

--- src/Homework/homeworks/Homework31.js
+++ src/Homework/homeworks/Homework31.js
@@ -1,4 +1,4 @@
-import { GeoGebra } from '../../GeoGebra/GeoGebra.js';
+import { GeoGebra } from './GeoGebra/GeoGebra.js';
 import { WeedSvg } from '../../WeedSvg.js';
 
 export const Homework31  = () => `
