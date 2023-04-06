77d9f64de56f6b388c313b52316997143e057265
DarthVanger <darthvanger@gmail.com>
2023-04-06 23:30:57 +0300

    Add tipo-react video to Novoe

--- src/HomePage/Novoe/Novoe.js
+++ src/HomePage/Novoe/Novoe.js
@@ -1,9 +1,21 @@
+import { Homework41 } from '../../Homework/homeworks/Homework41.js';
+
 export const Novoe = () => {
   const element = document.createElement('div');
   element.id = 'novoe';
   
   element.innerHTML = `
     <h2 class="h2">Novoe</h2>
+    <figure class="figure">
+      <video controls>
+        <source src="/video/tipo-react.mp4" type="video/mp4">
+      </video>
+      <figcaption>Tipo React Tutorial By Van4ik</figcaption>
+    </figure>
+
+    ${Homework41()}
+
+    <h2 class="h2">Staroe</h2>
     <figure class="figure">
       <video controls>
         <source src="/video/devtools-performance-tutorial.mp4" type="video/mp4">
