8ff534296e4e3c51c8e1b352c7743b8c442c5e7c
DarthVanger <darthvanger@gmail.com>
2023-04-17 00:23:43 +0300

    Quests: add 2 more steps to svg quest

--- src/quests/quests/svg.js
+++ src/quests/quests/svg.js
@@ -30,20 +30,61 @@ export const steps = [
   Перший крок - додаємо пустий тег <code>&lt;svg&gt;</code> розміром <code>100х100</code> - це наша коробочка де ми будем малювати Векторну Графіку, що Розтягується (ВГщР).
 </p>
 <pre><code>
-  <svg width="100" height="100"></svg>
+  &lt;svg width="100" height="100"&gt;
 </code></pre>
 `,
     check: `Уписано &lt;svg width="100" height="100"&gt;&lt;/svg&gt;`,
-    regexp: /<svg\s+width="100"\s+height="100"\s*>\s*<\/svg>/
+    regexp: /<svg\s+width="100"\s+height="100"\s*>/
   },
   {
     task: `
 <p>
-  Молодца. Теперь <code>while</code> пустой пишем <code>while (false) {}</code>. Синтоксис такой же как у <code>if () {}</code> да и на синтоксес функцей похож тоже :). В круглые скобки <code>false</code> мы пишем пока что просто так, чтобы не было ошибки - пустое условие быть не может. Потом заменим <code>false</code>на настоящее условие.
+  Другий крок - успередині тегу <code>svg</code> пишемо тег <code>&lt;circle /&gt;</code> з атрибутами <code>cx</code>, <code>cy</code>, <code>r</code>, <code>fill</code> (Слеш <code>/</code> укінці тегу <code>/&gt;</code> &mdash; обов'язково!)
+</p>
+<pre><code>
+  &lt;svg width="100" height="100"&gt;
+    &lt;circle cx="50" cy="50" r="40" fill="yellow" /&gt;
+  &lt;/svg&gt;
+</code></pre>
+`,
+    check: `Уписано &lt;circle cx="50" cy="50" r="40" fill="yellow" /&gt; усередині svg`,
+    regexp: /<circle\s+cx="\d+"\s+cy="\d+"\s+r="\d+"\s+fill="[^"]+" ?\/>/
+  },
+  {
+    task: `
+<p>
+  Мав зявитися жовтий кружечок на екрані.
+</p>
+  Тепер намалюй лінію
+<pre><code>
+&lt;svg width="100" height="100"&gt;
+  &lt;circle cx="50" cy="50" r="40" fill="yellow" /&gt;
+  &lt;line x1="0" y1="0" x2="100" y2="100" stroke="red" stroke-width="4" /&gt;
+&lt;/svg&gt;
+</code></pre>
 </p>
 `,
-    check: `Уписано <code>while (false) {}</code>`,
-    regexp: /while\s*[(]\s*(false|skokoRaz < 100?)\s*[)]\s*{[^}]*}/
+    check: `Уписано &lt;line x1="0" y1="0" x2="100" y2="100" stroke="red" stroke-width="4" /&gt; усередині svg`,
+    regexp: /<line\s+x1="0"\s+y1="\d+"\s+x2="\d+"\s+y2="\d+"\s+stroke="[^"]+"\s+stroke-width="\d+"\s*\/?>/
+  },
+  {
+    task: `
+<p>
+  Лінія мала зявитися по діагоналі: від точки <code>x1: 0; y1: 0</code> до точки <code>x2: 100; y2: 100</code>.
+</p>
+<p>
+  Тепер Хрестик давай :)
+<pre><code>
+&lt;svg width="100" height="100"&gt;
+  &lt;circle cx="50" cy="50" r="40" fill="yellow" /&gt;
+  &lt;line x1="0" y1="0" x2="100" y2="100" stroke="red" stroke-width="4" /&gt;
+  &lt;line x1="100" y1="0" x2="0" y2="100" stroke="red" stroke-width="4" /&gt;
+&lt;/svg&gt;
+</code></pre>
+</p>
+`,
+    check: `Уписано &lt;line x1="100" y1="0" x2="0" y2="100" stroke="red" stroke-width="4" /&gt; усередині svg`,
+    regexp: /<line\s+x1="100"\s+y1="\d+"\s+x2="\d+"\s+y2="\d+"\s+stroke="[^"]+"\s+stroke-width="\d+"\s*\/?>/
   },
 ];
 
