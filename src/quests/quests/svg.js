5f06f6f26cc09ed3f02ea292bca658bd553c86c9
DarthVanger <darthvanger@gmail.com>
2023-04-17 00:53:38 +0300

    Quests: svg - add 5th step

--- src/quests/quests/svg.js
+++ src/quests/quests/svg.js
@@ -86,9 +86,28 @@ export const steps = [
     check: `Уписано &lt;line x1="100" y1="0" x2="0" y2="100" stroke="red" stroke-width="4" /&gt; усередині svg`,
     regexp: /<line\s+x1="100"\s+y1="\d+"\s+x2="\d+"\s+y2="\d+"\s+stroke="[^"]+"\s+stroke-width="\d+"\s*\/?>/
   },
+  {
+    task: `
+<p>
+  Есть Хрестик?) Што ж, давай, прямоугольник!
+</p>
+<p>
+<pre><code>
+&lt;svg width="100" height="100"&gt;
+  &lt;circle cx="50" cy="50" r="40" fill="yellow" /&gt;
+  &lt;line x1="0" y1="0" x2="100" y2="100" stroke="red" stroke-width="4" /&gt;
+  &lt;line x1="100" y1="0" x2="0" y2="100" stroke="red" stroke-width="4" /&gt;
+  &lt;rect x="20" y="20" width="40" height="40" stroke="blue" stroke-width="4" /&gt;
+&lt;/svg&gt;
+</code></pre>
+</p>
+`,
+    check: `Уписано &lt;rect x="20" y="20" width="40" height="40" stroke="blue" stroke-width="4" /&gt; усередині svg`,
+    regexp: /<rect\s+x="\d+"\s+y="\d+"\s+width="\d+"\s+height="\d+"\s+stroke="[^"]+"\s+stroke-width="\d+"\s*\/?>/
+  },
 ];
 
-const skills = ['svg', 'svg-circle', 'svg-line'];
+const skills = ['svg', 'svg-circle', 'svg-line', 'svg-rect'];
 
 export default {
   story,
