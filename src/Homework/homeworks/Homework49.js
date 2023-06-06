5e83f2e183f30df060d029c167a168f9a139f741
DarthVanger <darthvanger@gmail.com>
2023-06-07 01:36:50 +0300

    Homework49: add link to djinni

--- src/Homework/homeworks/Homework49.js
+++ src/Homework/homeworks/Homework49.js
@@ -3,7 +3,7 @@ export const Homework49 = () => `
 
   <h3>Подаемся на вакансии</h3>
   <p>
-    Каждый день смотрим новые вакансии категории <em>Javascript / Frontend, Без Опыта</em>.
+    Каждый день смотрим новые вакансии на Djinni в категории <a href="https://djinni.co/jobs/?all-keywords=&any-of-keywords=&exclude-keywords=&primary_keyword=JavaScript&exp_level=no_exp">Javascript / Frontend, Без Опыта</a>.
   </p>
 
   <h3>Курсач - Второй Семестр</h3>
