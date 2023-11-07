53eb01006e60a94111e01ae835a6d3816086ea12
DarthVanger <darthvanger@gmail.com>
2023-11-07 20:07:47 +0200

    Show each student avatar on Home Page

--- src/HomePage/Students/StudentCard.js
+++ src/HomePage/Students/StudentCard.js
@@ -2,10 +2,13 @@ import { Avatar } from '../../Avatar.js';
 
 export const StudentCard = (state) => {
   const element = document.createElement('article');
+  element.className = 'student-card';
+
+  const { student } = state;
 
   element.innerHTML = `
-    <h2>Studetn card</h2>
-    ${state.student}
+    <h2>Studetn card ${student}</h2>
+    ${Avatar({student})}
   `;
 
   return element;
